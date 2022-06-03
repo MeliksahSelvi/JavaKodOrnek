434d304cd77a8720476f7e9e1189162bfe0c9435
MeliksahSelvi <106533983+MeliksahSelvi@users.noreply.github.com>
2022-06-03 16:03:07 +0300

    Initial commit

---
+++ src/Giris/Baslangic.java
@@ -0,0 +1,33 @@
+package Giris;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Baslangic {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int mat,turkce,fizik,kimya,biyo,tarih;
+        double ortalama;
+        String sonuc;
+
+        System.out.print("Matematik Notunuzu Giriniz :");
+        mat= input.nextInt();
+        System.out.print("Turkce Notunuzu Giriniz :");
+        turkce=input.nextInt();
+        System.out.print("Fizik Notunuzu Giriniz :");
+        fizik= input.nextInt();
+        System.out.print("Kimya Notunuzu Giriniz :");
+        kimya= input.nextInt();
+        System.out.print("Biyoloji Notunuzu Giriniz :");
+        biyo= input.nextInt();
+        System.out.print("Tarih Notunuzu Giriniz :");
+        tarih= input.nextInt();
+
+        ortalama=(mat+turkce+fizik+kimya+biyo+tarih)/6;
+
+        sonuc=ortalama >=60 ? "Gectiniz":"Gecemediniz";
+
+        System.out.println("Ortalamaniz : "+ortalama);
+        System.out.println("Donem Gecme Durumunuz : "+ sonuc);
+
+    }
+}
